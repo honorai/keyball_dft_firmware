0f55a26aeb779dd9a81aae42d57c9f2736380f75
honorai <186398080+honorai@users.noreply.github.com>
2025-07-29 03:05:28 +0900

    Create keymap.h

---
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.h
@@ -0,0 +1,14 @@
+
+#include "quantum.h"  // QMKの基本定義を含む
+
+// Tap Danceのインデックス（TD()マクロで使用）
+enum {
+    TD_LNG12,
+    TD_7_SMINS,
+    TD_CL_ALLCL,
+};
+
+// TD() マクロを使いやすくするための定義
+#define TD_1 TD(TD_LNG12)
+#define TD_2 TD(TD_7_SMINS)
+#define TD_3 TD(TD_CL_ALLCL)
