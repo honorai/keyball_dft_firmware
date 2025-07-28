39b7183f4dfc38a279b880cda57431fb0fb541c1
honorai <186398080+honorai@users.noreply.github.com>
2025-07-29 01:48:33 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
@@ -26,24 +26,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   //vase
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
-    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7)  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_INT1  ,
-              KC_LALT,KC_LGUI,KC_LCTL     ,LT(1,KC_SPC),LT(2,KC_DEL),                  KC_BSPC,LT(4,KC_ENT), RCTL_T(KC_LNG2),     KC_RALT  , KC_PSCR
+    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_EQL   ,
+    KC_LSFT   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_MINS  ,
+    KC_LCTL  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_BSLS  ,
+              KC_LALT,KC_LGUI,KC_LCTL     ,LT(1,KC_SPC),LT(2,KC_DEL),                             KC_BSPC,LT(4,KC_ENT), _______  ,   _______  , TD_LANGSWITCH
   ),
 //num
   [1] = LAYOUT_universal(
-    SSNP_FRE ,  KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,                                         KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-    SSNP_VRT ,  _______ , _______  , KC_UP   , KC_ENT   , KC_DEL   ,                                         KC_PGUP  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_BTN3  , KC_F12   ,
-    SSNP_HOR ,  _______ , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_BSPC  ,                                         KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , _______  ,
+    KC_ESC ,  KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,                                         KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , _______   ,
+    KC_LSFT ,  KC_1    , KC_2     , KC_3    , KC_4     , KC_5     ,                                   KC_6  , TD_7_UNDERSCORE  , KC_8    , KC_9  , KC_0  , _______   ,
+    KC_LCTL ,  _______ , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_BSPC  ,                                         KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , _______  ,
                   _______  , _______ , _______  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
   ),
  //symbol
   [2] = LAYOUT_universal(
     _______  ,S(KC_QUOT), KC_7     , KC_8    , KC_9     , S(KC_8)  ,                                         S(KC_9)  , S(KC_1)  , S(KC_6)  , KC_LBRC  , S(KC_4)  , _______  ,
-    _______  ,S(KC_SCLN), KC_4     , KC_5    , KC_6     , KC_RBRC  ,                                         KC_NUHS  , KC_MINS  , S(KC_EQL), S(KC_3)  , KC_QUOT  , S(KC_2)  ,
-    _______  ,S(KC_MINS), KC_1     , KC_2    , KC_3     ,S(KC_RBRC),                                        S(KC_NUHS),S(KC_INT1), KC_EQL   ,S(KC_LBRC),S(KC_SLSH),S(KC_INT3),
-                  KC_0     , KC_DOT  , _______  ,         _______  , _______  ,                   KC_DEL   , _______  , _______       , _______  , _______
+   _______ ,  S(KC_1)   , S(KC_2)  , S(KC_3) , S(KC_4)  , S(KC_5)  ,                                        S(KC_6)  , S(KC_7)  , S(KC_8)    , S(KC_9) , S(KC_0)  , _______   ,
+   _______  ,S(KC_MINS), KC_1     , KC_2    , KC_3     ,S(KC_RBRC),                                        S(KC_NUHS),S(KC_INT1), KC_EQL   ,S(KC_LBRC),S(KC_SLSH),S(KC_INT3),
+                  KC_0     , KC_DOT  , _______  ,         _______  , _______  ,                   _______   , _______  , _______       , _______  , _______
   ),
 //config
   [3] = LAYOUT_universal(
@@ -54,10 +54,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 //mouse
   [4] = LAYOUT_universal(
-    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , _______  ,                                        RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,                                        RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
-                  QK_BOOT  , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , QK_BOOT
+    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , _______  ,                                        _______  , _______  , KC_UP    , KC_INS , KC_PSCR , _______  ,
+    RGB_MOD  , _______  , _______  , CPI_T100  , CPI_T1K  , SCRL_DVI ,                                        _______  , KC_LEFT  , KC_DOWN  , KC_RGHT , _______  , _______  ,
+    RGB_RMOD , _______  , _______  , CPI_D100  , CPI_D1K  , SCRL_DVD ,                                        _______  , _______ , TD_CLOSE_ALLCLOSE , _______  , _______  , _______ ,
+                  QK_BOOT  , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
   ),
 };
 // clang-format on
@@ -102,4 +102,20 @@ combo_t key_combos[] = {
 [LT1LT2_MO3] = COMBO(my_lt1lt2, MO(3)),
 [LCTLT1_MO4] = COMBO(my_ctrllt1, MO(4)),
 };
+#endif
+
+
+#ifdef TAP_DANCE_ENABLE
+enum {
+  TD_LANGSWITCH = 0,
+  TD_7_UNDERSCORE,
+  TD_CLOSE_ALLCLOSE,
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_LANGSWITCH] = ACTION_TAP_DANCE_DOUBLE(LANG2, LANG1),
+    [TD_7_UNDERSCORE] = ACTION_TAP_DANCE_DOUBLE(KC_7, S(KC_MINS)),
+    [TD_CLOSE_ALLCLOSE] = ACTION_TAP_DANCE_DOUBLE(C(KC_W),A(KC_F4)),
+};
+
 #endif
